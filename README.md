284bfea6501c99bb7a8e194cfba759796046173b
Vladyslav Gudzenko <vladislav.gudzenko@gmail.com>
2019-06-12 02:12:48 -0500

    Update Readme and bootstrap script

--- README.md
+++ README.md
@@ -6,8 +6,35 @@ export TF_VAR_org_id=10xxxxxxxx777
 export TF_VAR_billing_account=03xxxx-xxxxxx-xxxDDD
 export TF_VAR_env_folder=4xxxxxxxxxx5
 ```
+## Set Project IDs to monitor with Stackdriver
+```
+export CORE_PROJECT_ID="gke-private-svpc-service"
+export STACKDRIVER_PROJECT_ID="gke-private-svpc-service"
+export KUBE_PROJECT_ID="gke-private-demo1"
+export KUBE_CLUSTER="gke-private-demo1"
+```
+## To use Stackdriver, your project must be in a Stackdriver workspace
+```
+open "https://console.cloud.google.com/monitoring?project=${CORE_PROJECT_ID}"
+```
+
+*	Add Google Cloud Platform projects to monitor - select assosiated projects - click Continue
+*	Monitor AWS accounts - Skip AWS Setup
+*	Install the Stackdriver Agents - click Continue
+*	Get Reports by Email - select No reports then Continue.
+*	You see the message "Gathering information..." at the top of the page. Click Launch monitoring when it finishes:
+
+
 ## Bootstrap TF project and state bucket
 ```
 ./bootstrap_tf_project.sh
 source .tf_env
 ```
+## Deploy Stackdriver Alert Policies and Uptime checks
+```
+cd tf
+terraform init
+terraform plan
+terraform apply
+```
+
--- bootstrap_tf_project.sh
+++ bootstrap_tf_project.sh
@@ -5,7 +5,7 @@
 # export TF_VAR_billing_account=03xxxx-xxxxxx-xxxDDD
 # export TF_VAR_env_folder=4xxxxxxxxxx5
 
-BACKEND_CONFIG="backend.tfvars"
+BACKEND_CONFIG="tf/backend.tf"
 
 #Load environment variables
 [ -z "$1" ] && ENV_FILE=".tf_env" || ENV_FILE=$1
@@ -20,18 +20,54 @@ if [ -z "$TF_ADMIN" ]; then
   echo "Generating new TF Admin Project ID.."
   TF_VAR_random_id=$((100000 + RANDOM % 999999))
   TF_ADMIN="tf-admin-${TF_VAR_random_id}"
-  TF_USER="tfadmin"
   TF_VAR_state_bucket="tf-state-${TF_VAR_random_id}"
-  TF_IAM="${TF_USER}@${TF_ADMIN}.iam.gserviceaccount.com"
-  TF_ACCOUNT="serviceAccount:$TF_IAM"
 fi
+
+TF_USER="tfadmin"
+TF_IAM="${TF_USER}@${TF_ADMIN}.iam.gserviceaccount.com"
+TF_ACCOUNT="serviceAccount:$TF_IAM"
+OPS_ADMIN=$(gcloud config list account --format "value(core.account)")
 
 echo
 echo Current organization id: $TF_VAR_org_id
 echo Folder for projects:     $TF_VAR_env_folder
+echo Ops Admin e-mail:        $OPS_ADMIN
 echo TF Admin project id:     $TF_ADMIN
 echo TF Admin account:        $TF_IAM
 echo
+
+echo Updating $BACKEND_CONFIG ...
+cat > "$BACKEND_CONFIG" <<EOF
+  terraform {
+    backend "gcs" {
+      prefix  = "terraform/state/stackdriver"
+      project = "$TF_ADMIN"
+      bucket  = "$TF_VAR_state_bucket"
+    }
+  }
+EOF
+
+# Write environment variables into dot-env file
+echo Saving $ENV_FILE ...
+cat > "$ENV_FILE" <<EOF
+export TF_VAR_org_id=$TF_VAR_org_id
+export TF_VAR_billing_account=$TF_VAR_billing_account
+export TF_VAR_env_folder=$TF_VAR_env_folder
+export TF_VAR_admin_email=$OPS_ADMIN
+
+export TF_VAR_state_bucket=$TF_VAR_state_bucket
+export TF_ADMIN=$TF_ADMIN
+export GOOGLE_APPLICATION_CREDENTIALS=~/.gcp/${TF_ADMIN}.json
+
+export TF_VAR_stackdriver_project=$STACKDRIVER_PROJECT_ID
+export TF_VAR_core_project=$CORE_PROJECT_ID
+export TF_VAR_kube_project=$KUBE_PROJECT_ID
+EOF
+
+echo
+echo -n "Create TF admin project (y/n)? "
+read -t 3 answer
+[ "$answer" != "y" -a "$answer" != "Y" ] && echo "Exiting." && exit 0
 
 # Create terraform admin project
   gcloud projects create ${TF_ADMIN} --folder $TF_VAR_env_folder --set-as-default
@@ -71,21 +107,3 @@ echo
 # Create the remote bucket in Cloud Storage (For Terraform remote state)
   gsutil mb -p ${TF_ADMIN} gs://${TF_VAR_state_bucket}
   gsutil versioning set on  gs://${TF_VAR_state_bucket}
-
-echo Updating $BACKEND_CONFIG ...
-cat > "$BACKEND_CONFIG" <<EOF
-project = "$TF_ADMIN"
-bucket = "$TF_VAR_state_bucket"
-EOF
-
-# Write environment variables into dot-env file
-echo Saving $ENV_FILE ...
-cat > "$ENV_FILE" <<EOF
-export TF_VAR_org_id=$TF_VAR_org_id
-export TF_VAR_billing_account=$TF_VAR_billing_account
-export TF_VAR_env_folder=$TF_VAR_env_folder
-export TF_VAR_state_bucket=$TF_VAR_state_bucket
-export TF_ADMIN=$TF_ADMIN
-export GOOGLE_APPLICATION_CREDENTIALS=~/.gcp/${TF_ADMIN}.json
-EOF
-
